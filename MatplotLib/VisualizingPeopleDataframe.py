4c75c56f341c063e4d6199c7bfd249052f3139ff
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-12 13:35:50 -0600

    Added: Bar plot to visualize population per city.

--- MatplotLib/VisualizingPeopleDataframe.py
+++ MatplotLib/VisualizingPeopleDataframe.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('LearningPythonDataAnalysis/DataSets/people_data.csv')
 print(df)
 
+"""Number of people per gender"""
 # Count the number of rows per gender
 gender_counts = df['Gender'].value_counts()
 
@@ -16,4 +17,30 @@ plt.ylabel('People')
 plt.title('People per Gender')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
+plt.show()
+
+"""Number of people per city"""
+# Count the number of rows per city
+city_counts = df['City'].value_counts()
+print(city_counts)
+
+# Visualizing city_counts in a Bar plot
+plt.figure(figsize=(6, 4))
+bars = plt.bar(city_counts.index, city_counts.values, color=['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'brown', 'gold', 'royalblue'])
+plt.xlabel('City')
+plt.ylabel('Population')
+plt.title('Cities Population')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+for bar in bars:
+    height = bar.get_height()
+    plt.text(
+        bar.get_x() + bar.get_width() / 2,
+        height,
+        str(height),
+        ha='center',
+        va='bottom',
+        fontsize=9,
+        fontweight='bold'
+    )
+plt.xticks(rotation=65)
 plt.show()
