eaa1df3964d7618e57915e18a71ee3306a9e66a9
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-13 13:50:21 -0600

    Ocupations per gender seaborn visualization with heatmap from a pivot table.

--- Seaborn/seaborn_heatmap_from_pivot_table.py
+++ Seaborn/seaborn_heatmap_from_pivot_table.py
@@ -27,3 +27,25 @@ plt.ylabel('Name')
 plt.xticks(rotation=45, ha='right')  # Rotate city labels for readability
 plt.tight_layout()
 plt.show()
+
+# Load the dataset
+df = pd.read_csv('../DataSets/people_data.csv')
+ocupations_per_gender_counts = df.pivot_table(index='Ocupation', columns='Gender', aggfunc='size', fill_value=0)
+
+# Heatmap of pivot table
+plt.figure(figsize=(14, 8))
+sns.heatmap(
+    ocupations_per_gender_counts,
+    annot=True,           # Show the count numbers in each cell
+    fmt='d',              # Format as integers
+    cmap='viridis',        # Color palette (coolwarm, magma, viridis)
+    linewidths=0.5,       # Lines between cells
+    cbar_kws={'label': 'Ocupations Count'}  # Colorbar label
+)
+
+plt.title("Ocupations count per gender", fontsize=16)
+plt.xlabel('Gender')
+plt.ylabel('Ocupation')
+plt.xticks(rotation=45, ha='right')  # Rotate city labels for readability
+plt.tight_layout()
+plt.show()
