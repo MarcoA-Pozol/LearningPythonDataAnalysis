2fc04797085e10bb84d46fadac56b95ee03356fa
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-17 15:37:03 -0600

    Dataframe split example.

---
+++ Pandas/Series/split_dataframe_into_series.py
@@ -0,0 +1,25 @@
+"""Split a DataFrame into Series"""
+import pandas as pd
+
+
+df = pd.DataFrame(data={'name':['Jarid', 'Helena', 'Humberto', 'Angel', 'Dwayne'], 'age':[21, 19, 40, 64, 35], 'ocupation':['Medic', 'Dishwasher', 'Baker', 'Periodist', 'Engineer']})
+print(df)
+
+# Spliting dataframe into series
+names = df['name']
+ages = df['age']
+ocupations = df['ocupation']
+print(names, ages, ocupations)
+
+# Thesen are the same than the lists we have over, they are already pd.Series 
+name_series = pd.Series(names)
+age_series = pd.Series(ages)
+ocupation_series = pd.Series(ocupations)
+
+print(name_series, age_series, ocupation_series)
+
+# Using name_series as custom index for a dataframe
+df.index = name_series
+df.data = {'age':age_series, 'ocupation':ocupation_series}
+df = df.drop(columns=['name'])
+print(df)
