dcb28f09404a542db802d12ce40e358378e8aee9
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-17 12:28:38 -0600

    Loading a XLS file

--- Pandas/flights_dataframe.py
+++ Pandas/flights_dataframe.py
@@ -1,5 +1,8 @@
+# Importamos pandas con el alias "pd"
 import pandas as pd
 
-# Load dataset
-df = pd.read_csv("DataSets/flights_details.csv")
-print(df.describe())
+# Cargamos el set de datos a partir de un archivo xls (Excel) en un dataframe de pandas
+df = pd.read_excel("DataSets/flights_details.xls")
+
+print(df) # Visualizamos el dataframe (Nos muestra los datos en un formato similar a una tabla)
+print(df.describe()) # Nos muestra datos importantes de manera directa y sencilla como std, mean, max, min, etc
