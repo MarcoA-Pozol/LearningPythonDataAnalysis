35d7eca12fcdbbf9e19f75e17d1a68a6e0edf88f
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-20 16:33:05 -0600

    Drop occupations table, fix duplicated values and constrains updated.

--- DataBases/TechStore/create_occupations_table.sql
+++ DataBases/TechStore/create_occupations_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Occupations(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(150),
+    name VARCHAR(150) UNIQUE NOT NULL,
     avg_salary NUMERIC(10, 2) NOT NULL DEFAULT 800,
     avg_gender VARCHAR(15) NOT NULL DEFAULT 'Male' CHECK(avg_gender IN ('Male', 'Female', 'Other', 'I prefer not to say'))
 );
---
+++ DataBases/TechStore/removing_duplicated_occupations.sql
@@ -0,0 +1,16 @@
+-- Obtain every duplicated occupation row's name (Example: Electric Engineer | 2 & Desntist | 2)
+SELECT name, COUNT(*)
+FROM Occupations
+GROUP BY name
+HAVING COUNT(*) > 1;
+
+-- Removing duplicated occupations
+WITH duplicates AS (
+    SELECT MIN(id) as min_id, name
+    FROM Occupations
+    GROUP BY name
+    HAVING COUNT(*) > 1
+)
+DELETE FROM Occupations
+WHERE id NOT IN (SELECT min_id FROM duplicates)
+AND name IN (SELECT name FROM duplicates)
