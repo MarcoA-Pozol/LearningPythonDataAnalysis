8c18ed5ed6c5e9058380d237a2ab2d25ff3be373
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-21 16:36:50 -0600

    Database creation script was additioned with the use of validation for an existing database and try-except clausules.

---
+++ SQL/FlightsDataBase/create_flights_database.py
@@ -0,0 +1,34 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Load .env variables
+load_dotenv()
+
+try:
+    # Connect to default postgres database to create a new one
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user=os.getenv("DEFAULT_DB_USER"),
+        password=os.getenv("DEFAULT_DB_PASSWORD"),
+        host="localhost",
+        port="5432"
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+    print('Succesfully connected to postgres database!')
+except Exception as e:
+    print(f'Error during connecting to default postgres database: {e}')
+    raise e
+
+try:
+    # Create FlightsDB
+    db_name = "FlightsDB"
+    cursor.execute(f'CREATE DATABASE IF NOT EXISTS {db_name};')
+    print('New database with name {} was succesfully created!'.format(db_name,))
+except Exception as e:
+    print(f'Error during creating a new database with name {db_name}: {e}')
+    raise e
+finally:
+    cursor.close()
+    conn.close()
